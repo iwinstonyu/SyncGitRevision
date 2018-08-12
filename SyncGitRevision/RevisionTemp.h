db2e1952ce3a09cb68cbf766e67377962766e0ee
windpenguin <windpenguin@gmail.com>
2018-08-12 14:24:05 +0800

    提交代码

---
+++ SyncGitRevision/RevisionTemp.h
@@ -0,0 +1,21 @@
+//
+//	<one line to give the program's name and a brief idea of what it does.>
+//	Copyright (C) 2018. NetDragon. All rights reserved.
+//
+//	Created: 2018/8/9 10:34:25
+//	Version: 1.0
+//  Author: WenJin Yu
+//
+//
+#ifndef _SLG_REVISION_H_
+#define _SLG_REVISION_H_
+namespace lp 
+{
+//
+// Sync from git
+//
+#define SLG_BRANCH_NAME					"$SLG_BRANCH_NAME$"
+#define SLG_COMMIT_HASH					"$SLG_COMMIT_HASH$"
+#define SLG_COMMIT_DATE					"$SLG_COMMIT_DATE$"
+}
+#endif
